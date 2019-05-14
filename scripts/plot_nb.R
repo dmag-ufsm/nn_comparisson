fbd8149e698a0b633d0b747b3baadc2829e49876
Julio Pinto Coelho <juliopcrj@gmail.com>
2019-05-13 22:34:43 -0300

    Scripts [2]

--- scripts/plot_nb.R
+++ scripts/plot_nb.R
@@ -10,44 +10,28 @@ setwd("workspace/nn_comparisson/results/")
 files = list.files()
 files = grep("NB[0-9]*L[0-9]*.csv", files, perl = T, value = T)
 
-index = 1
-values = c()
-for(i in files){
+plots = c()
+p = 1
+for(i in 1:32){
+  fi = grep(paste("NB", i, "L[0-9]*.csv", sep=""), files, perl=T, value = T)
+  index = 1
+  best = c()
+  avg = c()
+  worst = c()
   try({
-    a = read.csv(i)
-    values[index] = min(a$MSE)
-    index = index + 1
+    for( j in fi ){
+      a = read.csv(j)
+      best[index] = min(a$MSE)
+      avg[index] = mean(a$MSE)
+      worst[index] = max(a$MSE)
+      index = index + 1
+    }
   })
+  dm = data.frame(rbind(best, avg, worst))
+  plots[p] = plot(dm)
 }
 
-ind = paste(files)
-ids = seq(1:length(files))
-
-# For some reason, values is starting at 2.
-data = data.frame(id=ids, 
-                  individual = ind,
-                  value=values)
-
-label_data = data
-bar_number = nrow(label_data)
-angle=90-360*(label_data$id-0.5)/bar_number
-label_data$hjust<-ifelse(angle < -90, 1, 0)
-label_data$angle<-ifelse(angle<-90, angle+180, angle)
-
-p = ggplot(data, aes(x=as.factor(ids), y=values))+
-  geom_bar(stat="identity", fill=alpha("skyblue", 0.7))+
-  ylim(-10, 50)+
-  theme_minimal()+
-  theme(
-    axis.text=element_blank(),
-    axis.title = element_blank(),
-    panel.grid = element_blank(),
-    plot.margin = unit(rep(-1,4), "cm")
-  )+
-  coord_polar(start=0)+
-  geom_text(data=label_data, aes(x=ids, y=value+10, label=individual, hjust=hjust), color="black", fontface="bold", alpha=0.6, size=2.6, angle=label_data$angle, inherit.aes = F)
-
-
-# for testing:
-#p = ggplot(data)
-p
+# wat
+#data = data.frame(id=ids, 
+#                  individual = ind,
+#                  value=values)
